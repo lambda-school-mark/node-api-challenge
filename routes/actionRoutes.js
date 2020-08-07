314e70ffd96ed4515d9f4c31cbd70fa4a54ac833
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-07 13:21:35 -0400

    action create

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -4,6 +4,19 @@ const router = express.Router();
 
 const Actions = require("../data/helpers/actionModel");
 
+//create
+router.post("/", (req, res) => {
+  Actions.insert(req.body)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) =>
+      res
+        .send(500)
+        .json({ errorMessage: "unable to create action", error: err })
+    );
+});
+
 //read
 router.get("/", (req, res) => {
   Actions.get()
